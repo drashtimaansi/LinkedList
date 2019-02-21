dccae9b009ccc18abf20569bd73f84cde45aac81
drashtimaansi <drashti.maheshwari_da17@gla.ac.in>
2019-02-21 08:42:36 +0530

    Add constructor for data

---
+++ src/Day_2/SinglyLinkedList.java
@@ -0,0 +1,23 @@
+package Day_2;
+
+public class SinglyLinkedList {
+    private Node head=null;
+    private int size=0;
+
+
+
+    private static class Node{
+        private int data;
+        private Node next;
+
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+}
